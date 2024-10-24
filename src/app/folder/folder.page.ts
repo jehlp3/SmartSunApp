b6d96ba84cb85c851d7ef08d988a78a950ca8e57
GitAlvaro-student <cardocardin@gmail.com>
2024-10-23 22:31:14 -0300

    Inputs e Botão de Cálculo da Tela Principal do App

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,10 +8,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
+  public numero1!: number;
+  public numero2!: number;
+  public resultado!: number;
   private activatedRoute = inject(ActivatedRoute);
+  private calculadora = new Calculadora(); // Instancia da classe Calculadora
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  calcularGastoMedio() {
+    if (this.numero1 !== undefined && this.numero2 !== undefined) {
+      const resultadoCalc = this.calculadora.calcular(this.numero1, this.numero2);
+      this.resultado = parseFloat(resultadoCalc.toFixed(2)); // Retorna com 2 casas decimais e converte para número
+    } else {
+      this.resultado = 0;
+    }
+  }
+}
+
+// Classe Calculadora
+export class Calculadora {
+  calcular(a: number, b: number): number {
+    return a !== 0 ? b / a : 0;
   }
 }
